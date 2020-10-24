23a74551a9869a96468b9ec65a370d7f2a26a128
MariaMB17 <marialva1705@gamil.com>
2020-10-24 18:54:36 -0430

    Se crean endPoint para logueo

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public $successStatus = 200;
+    //
+     /**
+     * Registro de usuario
+     */
+    public function signUp(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json([
+            'message' => 'Successfully created user!'
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('Password Token')-> accessToken; 
+            $success['name'] = $user->name;
+            $success['email'] = $user->email;
+            return response()->json(['success' => $success], $this-> successStatus); 
+        } 
+        else{ 
+            return response()->json(['error'=>'Unauthorised'], 401); 
+        } 
+    }
+
+    /** 
+     * details api 
+     * 
+     * @return \Illuminate\Http\Response 
+     */ 
+    public function user(Request $request) 
+    { 
+        $user = Auth::user(); 
+        return response()->json(['success' => $user], $this-> successStatus); 
+    }
+
+    public function logout (Request $request) {
+        $token = $request->user()->token();
+        $token->revoke();
+        $response = ['message' => 'You have been successfully logged out!'];
+        return $token;
+        //return response($response, 200);
+    }
+}
