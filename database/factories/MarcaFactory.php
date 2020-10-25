d8bb286c1330c5aa1ca1380e8145c154db02063c
MariaMB17 <marialva1705@gamil.com>
2020-10-25 16:55:02 -0430

    Se eleimina la migracion marca y se crea marcas

---
+++ database/factories/MarcaFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Marca;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MarcaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Marca::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'description' => $this->faker->sentence, 
+            'created_by' => 1,
+            'updated_by' => 1,
+        ];
+    }
+}
--- database/migrations/2020_10_25_211735_create_marcas_table.php
+++ database/migrations/2020_10_25_211735_create_marcas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateColumnsMarca extends Migration
+class CreateMarcasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,12 @@ class UpdateColumnsMarca extends Migration
      */
     public function up()
     {
-        Schema::table('marca', function (Blueprint $table) {
+        Schema::create('marcas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('description');
             $table->integer('created_by')->nullable()->index('created_by_idx');
             $table->integer('updated_by')->nullable()->index('updated_by_idx');
+            $table->timestamps();
         });
     }
 
@@ -26,16 +29,6 @@ class UpdateColumnsMarca extends Migration
      */
     public function down()
     {
-        if (Schema::hasColumn('marca', 'created_by')) {
-            Schema::table('marca', function (Blueprint $table) {
-                $table->dropColumn('created_by');
-            });
-        }
-        if (Schema::hasColumn('marca', 'updated_by')) {
-            Schema::table('marca', function (Blueprint $table) {
-                $table->dropColumn('updated_by');
-            });
-        }
-        
+        Schema::dropIfExists('marcas');
     }
 }
